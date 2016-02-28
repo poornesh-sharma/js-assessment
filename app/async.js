906ddd5403c807b7b793b616dcdd33c0a2e0c903
poornesh-sharma <poornesh4u@gmail.com>
2016-02-28 10:01:21 +0530

    Update async.js

--- app/async.js
+++ app/async.js
@@ -1,11 +1,33 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
+	async : function (value) {
+		return new Promise(function (resolve) {
+			resolve(value);
+		});
+	},
 
-  },
+	manipulateRemoteData : function (url) {
+		var promise = new Promise(function (resolve) {
+			// Instantiates the XMLHttpRequest
+			var client = new XMLHttpRequest();
+			var uri = url;
 
-  manipulateRemoteData : function(url) {
+			client.open("GET", uri);
+			client.send();
 
-  }
+			client.onload = function () {
+				if (this.status >= 200 && this.status < 300) {
+					var result = JSON.parse(this.response).people.sort(function(a,b) { return a.name.localeCompare(b.name);}).map(function(temp) { return temp.name;});
+					resolve(result);
+				} else {
+					reject(this.statusText);
+				}
+			};
+			client.onerror = function () {
+				reject(this.statusText);
+			};
+		});
+		return promise;
+	}
 };
